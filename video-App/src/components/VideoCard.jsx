08da9ac1d0856a088018907ce99fffb6b3292314
akshatsabavat <75850891+akshatsabavat@users.noreply.github.com>
2023-05-28 12:04:12 +0530

    cartStyle added

--- video-App/src/components/VideoCard.jsx
+++ video-App/src/components/VideoCard.jsx
@@ -1,56 +1,91 @@
 import { BsFillStopwatchFill } from "react-icons/bs";
 import { RiPlayList2Fill } from "react-icons/ri";
+import { MdVerified } from "react-icons/md";
 import { videoTitleGenerator } from "../helperfunctions/videoTitleGenerator";
 
 import { Box, Flex, Icon, Image, Text } from "@chakra-ui/react";
+import { useDispatch } from "react-redux";
+import { setCreatorDetails, setVideoDetails } from "../store/videoPlayerSlice";
+import { Link } from "react-router-dom";
 
-const VideoCard = ({ thumbnail, creatorName }) => {
+const VideoCard = ({
+  thumbnail,
+  creatorHandle,
+  videoDetails,
+  creatorDetails,
+  creatorPic,
+}) => {
+  const dispatch = useDispatch();
   return (
     <Box fontFamily="poppins">
-      <Box
-        h={"200px"}
-        w={"350px"}
-        borderRadius="15px"
-        boxShadow="rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset"
-        style={{ position: "relative", overflow: "hidden" }}
-      >
-        <Image
-          src={thumbnail}
-          alt="Video Thumbnail"
-          style={{
-            width: "100%",
-            height: "100%",
-            objectFit: "cover",
-            objectPosition: "center",
-          }}
-        />{" "}
+      <Link to={"/video"}>
         <Box
-          position="absolute"
-          bottom="0"
-          left="0"
-          w="100%"
-          h="100%"
-          p="4"
-          bgGradient="linear(to-t, rgba(0, 0, 0, 0.2), transparent)"
-          _before={{
-            content: '""',
-            position: "absolute",
-            top: "0",
-            left: "0",
-            right: "0",
-            bottom: "0",
-            backgroundColor: "rgba(0, 0, 0, 0.6)",
+          onClick={() => {
+            dispatch(setVideoDetails(videoDetails));
+            dispatch(setCreatorDetails(creatorDetails));
           }}
-        />
-        <Flex position="absolute" top="5" left="4" gap={3}>
-          <Icon as={BsFillStopwatchFill} color="white" boxSize={6} />
-          <Icon as={RiPlayList2Fill} color="white" boxSize={6} />
-        </Flex>
-      </Box>
-      <Text marginTop={4} fontSize={"xl"} fontWeight={500}>
-        {videoTitleGenerator()}
-      </Text>
-      <Text>{creatorName}</Text>
+          h={"200px"}
+          w={"350px"}
+          borderRadius="15px"
+          boxShadow="rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset"
+          style={{ position: "relative", overflow: "hidden" }}
+          _hover={{
+            cursor: "pointer",
+            transform: "scale(1.01)",
+          }}
+          transition="transform 0.3s ease-in-out"
+        >
+          <Image
+            src={thumbnail}
+            alt="Video Thumbnail"
+            style={{
+              width: "100%",
+              height: "100%",
+              objectFit: "cover",
+              objectPosition: "center",
+            }}
+          />{" "}
+          <Box
+            position="absolute"
+            bottom="0"
+            left="0"
+            w="100%"
+            h="100%"
+            p="4"
+            bgGradient="linear(to-t, rgba(0, 0, 0, 0.2), transparent)"
+            _before={{
+              content: '""',
+              position: "absolute",
+              top: "0",
+              left: "0",
+              right: "0",
+              bottom: "0",
+              backgroundColor: "rgba(0, 0, 0, 0.6)",
+            }}
+            _hover={{
+              opacity: 0,
+            }}
+            transition="opacity 0.2s ease-in-out"
+          />
+          <Flex position="absolute" top="5" left="4" gap={3}>
+            <Icon as={BsFillStopwatchFill} color="white" boxSize={6} />
+            <Icon as={RiPlayList2Fill} color="white" boxSize={6} />
+          </Flex>
+        </Box>
+      </Link>
+      <Flex marginTop="25px" alignItems={"flex-start"} gap={4}>
+        <Image height="40px" width="40px" borderRadius="50%" src={creatorPic} />
+        <Box>
+          <Text fontSize={"xl"} fontWeight={500}>
+            {videoTitleGenerator()}
+          </Text>
+          <Flex marginTop="3px" gap={2} alignItems={"center"}>
+            <Text fontSize="15px">{creatorHandle}</Text>
+            <Icon boxSize={5} color="blue.500" as={MdVerified} />
+          </Flex>
+          <Text fontSize="15px">46k views • 15 hours ago</Text>
+        </Box>
+      </Flex>
     </Box>
   );
 };
--- video-App/src/components/VideoList.jsx
+++ video-App/src/components/VideoList.jsx
@@ -1,22 +1,9 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchVideoAPI } from "../store/videoSlice";
 
-import {
-  Box,
-  Flex,
-  Grid,
-  Icon,
-  Image,
-  Text,
-  CircularProgress,
-} from "@chakra-ui/react";
-import { BsFillStopwatchFill } from "react-icons/bs";
-import { RiPlayList2Fill } from "react-icons/ri";
-import { videoTitleGenerator } from "../helperfunctions/videoTitleGenerator";
+import { Grid, CircularProgress } from "@chakra-ui/react";
 import VideoCard from "./VideoCard";
-import { Link } from "react-router-dom";
-import { setCreatorDetails, setVideoDetails } from "../store/videoPlayerSlice";
 
 const VideoList = () => {
   const dispatch = useDispatch();
@@ -39,72 +26,14 @@ const VideoList = () => {
           <CircularProgress color="red" isIndeterminate />
         ) : (
           data.map((video) => (
-            // <VideoCard
-            //   key={video.postId}
-            //   thumbnail={video.submission.thumbnail}
-            //   creatorName={video.creator.name}
-            // />
-            <Box key={video.postId} fontFamily="poppins">
-              <Link to={"/video"}>
-                <Box
-                  onClick={() => {
-                    dispatch(setVideoDetails(video.submission));
-                    dispatch(setCreatorDetails(video.creator));
-                  }}
-                  h={"200px"}
-                  w={"350px"}
-                  borderRadius="15px"
-                  boxShadow="rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset"
-                  style={{ position: "relative", overflow: "hidden" }}
-                  _hover={{
-                    cursor: "pointer",
-                    transform: "scale(1.01)",
-                  }}
-                  transition="transform 0.3s ease-in-out"
-                >
-                  <Image
-                    src={video.submission.thumbnail}
-                    alt="Video Thumbnail"
-                    style={{
-                      width: "100%",
-                      height: "100%",
-                      objectFit: "cover",
-                      objectPosition: "center",
-                    }}
-                  />{" "}
-                  <Box
-                    position="absolute"
-                    bottom="0"
-                    left="0"
-                    w="100%"
-                    h="100%"
-                    p="4"
-                    bgGradient="linear(to-t, rgba(0, 0, 0, 0.2), transparent)"
-                    _before={{
-                      content: '""',
-                      position: "absolute",
-                      top: "0",
-                      left: "0",
-                      right: "0",
-                      bottom: "0",
-                      backgroundColor: "rgba(0, 0, 0, 0.6)",
-                    }}
-                    _hover={{
-                      opacity: 0,
-                    }}
-                    transition="opacity 0.2s ease-in-out"
-                  />
-                  <Flex position="absolute" top="5" left="4" gap={3}>
-                    <Icon as={BsFillStopwatchFill} color="white" boxSize={6} />
-                    <Icon as={RiPlayList2Fill} color="white" boxSize={6} />
-                  </Flex>
-                </Box>
-              </Link>
-              <Text marginTop={4} fontSize={"xl"} fontWeight={500}>
-                {videoTitleGenerator()}
-              </Text>
-              <Text>{video.creator.handle}</Text>
-            </Box>
+            <VideoCard
+              creatorPic={video.creator.pic}
+              videoDetails={video.submission}
+              creatorDetails={video.creator}
+              key={video.postId}
+              thumbnail={video.submission.thumbnail}
+              creatorHandle={video.creator.handle}
+            />
           ))
         )}
       </Grid>
